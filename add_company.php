09efb26460112eac4f3232d219a8d21d3cd6261e
vijaypk376 <vijay.1cotocus@gmail.com>
2022-10-15 14:45:29 +0530

    added

--- add_company.php
+++ add_company.php
@@ -5,7 +5,6 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title></title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>CRUD for Company Name</title>
